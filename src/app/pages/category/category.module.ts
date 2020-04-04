b1a9351de3cc0e2b54e46c7ce1ec16edb1f5540b
jmolesco <48753318+jmolesco@users.noreply.github.com>
2020-04-04 23:04:02 +0800

    restr
    ere

--- src/app/pages/category/category.module.ts
+++ src/app/pages/category/category.module.ts
@@ -4,17 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryDetailComponent } from './category-detail/category-detail.component';
-import { FormsModule }   from '@angular/forms';
-import {SharedModule} from '@shared/shared.module';
+
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryDetailComponent],
-  exports: [CategoryListComponent, CategoryDetailComponent],
   imports: [
     CommonModule,
-    CategoryRoutingModule,
-    FormsModule,
-    SharedModule
+    CategoryRoutingModule
   ]
 })
 export class CategoryModule { }
